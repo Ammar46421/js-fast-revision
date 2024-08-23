33feb7d341037aa906e6e4e6c9c4415d896d1341
Ammar46421 <153704851+Ammar46421@users.noreply.github.com>
2024-08-23 18:45:26 +0500

    summary

---
+++ app.js
@@ -0,0 +1,13 @@
+var PaiseGMasti;
+
+while (true) {
+    PaiseGMasti = prompt("enter the reason why  you bought charging port from AliExpress? ")
+    if (PaiseGMasti === "Discount Jo Asro" ) {
+        break;
+    }else if(PaiseGMasti === "gurbat"){
+        break;
+    }
+    alert("sahi se batao")
+}
+alert("Ghareeb Bhana")
+
